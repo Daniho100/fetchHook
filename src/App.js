f115cfe8ec8194122ddecaeec89d5571aaad3129
Ojiaku Daniel <daniandre.dah@gmail.com>
2022-09-12 17:01:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import useFetch from './useFetch';
 
 function App() {
+  const BASE_URL = "https://inshortsapi.vercel.app/news?category=science";
+
+   const {data, loading, error} = useFetch(BASE_URL);
+
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Learning Custom Hooks</h2>
+      {loading && <h3>Loading...</h3>}
+      {error && <h3>Error: something went wrong</h3>}
+      
+      <div>
+         <pre>{JSON.stringify(data, undefined, 2)}</pre>
+      </div>
     </div>
   );
 }
